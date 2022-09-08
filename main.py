c9cfd11137892a60706c905e9ccc4399237297eb
Soogeun Park <bwmelon97@naver.com>
2022-09-08 20:34:38 +0900

    feat: implement upload files func

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, File, UploadFile
+import cv2
 
 app = FastAPI()
 
@@ -9,8 +10,21 @@ async def create_upload_files(
     reference_img: UploadFile = File(description="Reference Image"),
 ):
     # TODO: OpenCV로 이미지, 비디오 파일을 띄우기
+    # 입력 받은 파일 생성    
+    print(type(key_frame.file))
+    
+    f = open(key_frame.filename, "bw")
+    f.write(key_frame.file.read())
+    f.close()
 
-    print(original_video.file)
-    print(key_frame)
-    print(reference_img)
+    # 해당 이미지 파일 읽고 띄우기
+    key_frame = cv2.imread(key_frame.filename, cv2.IMREAD_UNCHANGED)
+    cv2.imshow('image', key_frame)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    # Model Prediction
+
+    # 
+
     return {"output_video": "hello world"}

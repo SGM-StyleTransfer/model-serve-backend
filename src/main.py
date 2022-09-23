c2159fb7382466b8b78a7f3b3747664acf9cb7fd
Soogeun Park <bwmelon97@naver.com>
2022-09-23 14:45:01 +0900

    feat: modify custom swap func to cv2.cvtColor

--- src/main.py
+++ src/main.py
@@ -10,15 +10,6 @@ app = FastAPI()
 def load_img_into_np_array(data):
     return np.array(Image.open(BytesIO(data)))
 
-# nd array의 b, r 채널 swap
-def swap_red_blue_of_np_array(image_array):
-    r = image_array[:, :, 0].copy()
-    b = image_array[:, :, 2].copy()
-    image_array[:, :, 0] = b
-    image_array[:, :, 2] = r
-
-    return image_array
-
 @app.post("/api/uploadfiles/")
 async def upload_files_and_return_video (
     original_video: UploadFile = File(description="Original Video"),
@@ -29,13 +20,13 @@ async def upload_files_and_return_video (
     # 받은 file을 numpy의 ndarray로 변환
     # 비디오 파일은 모델에서 어떤 타입으로 사용되는지 확인 필요
     key_frame = load_img_into_np_array(await key_frame.read())
-    swaped_key_frame = swap_red_blue_of_np_array(key_frame)
+    swaped_key_frame = cv2.cvtColor(key_frame, cv2.COLOR_BGR2RGB)
     
     reference_img = load_img_into_np_array(await reference_img.read())
-    swaped_reference_img = swap_red_blue_of_np_array(reference_img)
+    swaped_reference_img = cv2.cvtColor(reference_img, cv2.COLOR_BGR2RGB)
     
     mask_img = load_img_into_np_array(await mask_img.read())
-    swaped_mask_img = swap_red_blue_of_np_array(mask_img)
+    swaped_mask_img = cv2.cvtColor(mask_img, cv2.COLOR_BGR2RGB)
 
     # 변환한 ndarray를 openCV로 확인
     cv2.imshow('image', swaped_reference_img)
